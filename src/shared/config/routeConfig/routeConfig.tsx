b248b8a84aa15bdfc7e3dc8f47ca55db9bb98f2b
Korovkin Dmitry <jezusworking@gmail.com>
2023-07-22 00:13:39 +0400

    Конфигурация роутов

---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,26 @@
+import {RouteProps} from "react-router-dom";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
+
+export enum AppRoutes {
+    MAIN = 'main',
+    ABOUT = 'about',
+}
+
+export const RoutePaths: Record<AppRoutes, string> = {
+    [AppRoutes.MAIN]: '/',
+    [AppRoutes.ABOUT]: '/about',
+}
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+    [AppRoutes.MAIN]: {
+        element: <MainPage />,
+        path: RoutePaths[AppRoutes.MAIN],
+    },
+    [AppRoutes.ABOUT]: {
+        element: <AboutPage />,
+        path: RoutePaths[AppRoutes.ABOUT],
+    }
+}
+
+
