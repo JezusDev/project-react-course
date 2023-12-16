c11492407af9100426e54ad5ddfb688532250e24
korovkindm <korovkindm@63pokupki.ru>
2023-12-16 15:52:00 +0400

    redux toolkit init

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,10 @@
+import { Counter } from './ui/Counter';
+import { CounterSchema } from './model/types/counterSchema';
+import { counterActions, counterReducer } from './model/slice/counterSlice';
+
+export {
+    Counter,
+    CounterSchema,
+    counterActions,
+    counterReducer,
+};
--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,19 @@ import { ThemeProvider } from 'app/providers/ThemeProvider';
 import 'shared/config/i18n/i18n';
 import 'app/styles/index.scss';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 
 render(
     <React.StrictMode>
-        <BrowserRouter>
-            <ErrorBoundary>
-                <ThemeProvider>
-                    <App />
-                </ThemeProvider>
-            </ErrorBoundary>
-        </BrowserRouter>
+        <StoreProvider>
+            <BrowserRouter>
+                <ErrorBoundary>
+                    <ThemeProvider>
+                        <App />
+                    </ThemeProvider>
+                </ErrorBoundary>
+            </BrowserRouter>
+        </StoreProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
